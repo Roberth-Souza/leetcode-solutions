28d97cadd18c09cb56f15918f25e12f8a2142690
RobeTech <roberth.halk@gmail.com>
2026-09-23 13:36:25 -0300

    feat: add sort-colors solution

---
+++ medium/75.sort-colors.py
@@ -0,0 +1,25 @@
+# @leet start
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        low = 0
+        high = len(nums) - 1
+
+        mid = 0
+
+        while mid <= high:
+            if nums[mid] == 2:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
+            elif nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            else:
+                mid += 1
+
+
+# @leet end
